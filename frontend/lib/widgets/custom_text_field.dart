bf7f446ec5cc725a12f406f0d7eab214f62c2a7e
Aryan Nagori <aryannikill123@gmail.com>
2026-04-04 12:02:24 +0530

    enhance CustomTextField with glowing border animation

--- frontend/lib/widgets/custom_text_field.dart
+++ frontend/lib/widgets/custom_text_field.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../config/theme.dart';
-import '../config/constants.dart';
 
 class CustomTextField extends StatefulWidget {
   final String label;
@@ -39,17 +38,26 @@ class CustomTextField extends StatefulWidget {
 class _CustomTextFieldState extends State<CustomTextField>
     with SingleTickerProviderStateMixin {
   late AnimationController _animationController;
-  late Animation<double> _scaleAnimation;
+  late Animation<double> _focusAnimation;
+  late Animation<Color?> _borderColorAnimation;
   bool _isFocused = false;
 
   @override
   void initState() {
     super.initState();
     _animationController = AnimationController(
-      duration: AppConstants.durationMedium,
+      duration: const Duration(milliseconds: 400),
       vsync: this,
     );
-    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.02).animate(
+
+    _focusAnimation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+    );
+
+    _borderColorAnimation = ColorTween(
+      begin: AppColors.borderColor,
+      end: AppColors.primaryColor,
+    ).animate(
       CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
     );
   }
@@ -62,80 +70,109 @@ class _CustomTextFieldState extends State<CustomTextField>
 
   @override
   Widget build(BuildContext context) {
-    return ScaleTransition(
-      scale: _scaleAnimation,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            widget.label,
-            style: AppTextStyles.bodyLarge.copyWith(
-              fontWeight: FontWeight.w600,
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          widget.label,
+          style: AppTextStyles.bodyLarge.copyWith(
+            fontWeight: FontWeight.w600,
+            color: AppColors.textDark,
           ),
-          const SizedBox(height: 8),
-          Focus(
-            onFocusChange: (hasFocus) {
-              setState(() => _isFocused = hasFocus);
-              if (hasFocus) {
-                _animationController.forward();
-              } else {
-                _animationController.reverse();
-              }
+        ),
+        const SizedBox(height: 10),
+        Focus(
+          onFocusChange: (hasFocus) {
+            setState(() => _isFocused = hasFocus);
+            if (hasFocus) {
+              _animationController.forward();
+            } else {
+              _animationController.reverse();
+            }
+          },
+          child: AnimatedBuilder(
+            animation: _focusAnimation,
+            builder: (context, child) {
+              return Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  boxShadow: [
+                    if (_isFocused)
+                      BoxShadow(
+                        color: AppColors.primaryColor.withOpacity(
+                          0.2 * _focusAnimation.value,
+                        ),
+                        blurRadius: 12 * _focusAnimation.value,
+                        spreadRadius: 2 * _focusAnimation.value,
+                      ),
+                  ],
+                ),
+                child: TextFormField(
+                  controller: widget.controller,
+                  keyboardType: widget.keyboardType,
+                  obscureText: widget.obscureText,
+                  maxLines: widget.obscureText ? 1 : widget.maxLines,
+                  minLines: widget.minLines,
+                  readOnly: widget.readOnly,
+                  validator: widget.validator,
+                  onChanged: widget.onChanged,
+                  style: AppTextStyles.bodyLarge,
+                  decoration: InputDecoration(
+                    hintText: widget.hint,
+                    labelText: widget.label,
+                    prefixIcon: widget.prefixIcon != null
+                        ? Icon(
+                            widget.prefixIcon,
+                            color: _isFocused
+                                ? AppColors.primaryColor
+                                : AppColors.textLight,
+                            size: 20,
+                          )
+                        : null,
+                    suffixIcon: widget.suffixIcon != null
+                        ? Icon(
+                            widget.suffixIcon,
+                            color: _isFocused
+                                ? AppColors.primaryColor
+                                : AppColors.textLight,
+                            size: 20,
+                          )
+                        : null,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: const BorderSide(
+                        color: AppColors.borderColor,
+                        width: 1.2,
+                      ),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: AppColors.borderColor.withOpacity(0.8),
+                        width: 1.2,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14),
+                      borderSide: BorderSide(
+                        color: _borderColorAnimation.value ??
+                            AppColors.primaryColor,
+                        width: 2.0,
+                      ),
+                    ),
+                    fillColor: AppColors.backgroundColor,
+                    filled: true,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 18,
+                      vertical: 18,
+                    ),
+                  ),
+                ),
+              );
             },
-            child: TextFormField(
-              controller: widget.controller,
-              keyboardType: widget.keyboardType,
-              obscureText: widget.obscureText,
-              maxLines: widget.obscureText ? 1 : widget.maxLines,
-              minLines: widget.minLines,
-              readOnly: widget.readOnly,
-              validator: widget.validator,
-              onChanged: widget.onChanged,
-              style: AppTextStyles.bodyLarge,
-              decoration: InputDecoration(
-                hintText: widget.hint,
-                labelText: widget.label,
-                prefixIcon: widget.prefixIcon != null
-                    ? Icon(
-                        widget.prefixIcon,
-                        color: _isFocused
-                            ? AppColors.primaryColor
-                            : AppColors.textLight,
-                      )
-                    : null,
-                suffixIcon: widget.suffixIcon != null
-                    ? Icon(
-                        widget.suffixIcon,
-                        color: _isFocused
-                            ? AppColors.primaryColor
-                            : AppColors.textLight,
-                      )
-                    : null,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(AppConstants.radiusMedium),
-                  borderSide: const BorderSide(
-                    color: AppColors.borderColor,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(AppConstants.radiusMedium),
-                  borderSide: const BorderSide(
-                    color: AppColors.borderColor,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(AppConstants.radiusMedium),
-                  borderSide: const BorderSide(
-                    color: AppColors.primaryColor,
-                    width: 2,
-                  ),
-                ),
-              ),
-            ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
